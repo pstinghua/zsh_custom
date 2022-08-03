fddef1b30afdf98ea752449eb7e6e8cc24df3313
pstinghua <pstinghua@gmail.com>
2022-08-03 11:00:51 +0800

    Chores

--- cassandra.zsh
+++
@@ -1,1 +0,0 @@
-export CASSANDRA_URL='cds-uf60148k08k92p2h-1-core-001.cassandra.rds.aliyuncs.com'
--- flutter.zsh
+++ flutter.zsh
@@ -1,2 +1,5 @@
 export PUB_HOSTED_URL=https://pub.flutter-io.cn
 export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn
+
+alias fladd='flutter pub add'
+alias flcreate='flutter create'
